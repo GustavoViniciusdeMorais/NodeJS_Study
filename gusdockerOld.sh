ac3b752a276efa303de90ac85402f08dc58e3d4c
Gustavo <gustavocop14dos@gmail.com>
2023-11-19 11:51:09 -0300

    - build model of LSTM tfjs example

--- gusdockerOld.sh
+++ gusdockerOld.sh
@@ -24,6 +24,11 @@ then
             docker-compose down
         fi
 
+        if [ $1 = "--stop-all" ]
+        then
+            docker container stop $(docker container ls -aq) && docker network prune -f
+        fi
+
         if [ -n "$2" ]
         then
             docker exec -it -u 0 $1 $2
